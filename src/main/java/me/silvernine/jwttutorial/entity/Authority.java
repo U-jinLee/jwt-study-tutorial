5b6575040225166c63506874b15349b2def90736
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-03-21 22:13:17 +0900

    Initial commit

---
+++ src/main/java/me/silvernine/jwttutorial/entity/Authority.java
@@ -0,0 +1,21 @@
+package me.silvernine.jwttutorial.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "authority")
+@Entity
+public class Authority {
+    @Id
+    @Column(name="authority_name", length = 50)
+    private String authorityName;
+}
---
+++ src/main/java/me/silvernine/jwttutorial/entity/User.java
@@ -0,0 +1,40 @@
+package me.silvernine.jwttutorial.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name= "user")
+@Entity
+public class User {
+
+    @Id
+    @Column(name="user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long UserId;
+
+    @Column(name="username", length = 50, unique = true)
+    private String userName;
+
+    @Column(name="password", length = 100)
+    private String password;
+
+    @Column(name="nickname", length = 50)
+    private String nickname;
+
+    @Column(name="activated")
+    private boolean activated;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_autority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+}
