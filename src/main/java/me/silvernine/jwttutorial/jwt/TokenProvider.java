aa7bf67417a643b28c758ab2c25e9c5781e07909
yoojin Lee <leeujin1029@naver.com>
2022-03-22 11:21:28 +0900

    Feat: afterPropertiesSet 메서드 생성
    빈 생성 후 의존성 주입이 완료된 후에 주입받은 secret 값을 BaseDecode한 후 key 변수에 할당

--- src/main/java/me/silvernine/jwttutorial/jwt/TokenProvider.java
+++ src/main/java/me/silvernine/jwttutorial/jwt/TokenProvider.java
@@ -1,4 +1,33 @@
 package me.silvernine.jwttutorial.jwt;
 
-public class TokenProvider {
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+
+@Component
+public class TokenProvider implements InitializingBean {
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private final String secret;
+    private final long tokenValidityInMilliSeconds;
+
+    private Key key;
+
+    public TokenProvider(@Value("${jwt.secret}") String secret, @Value("${jwt.token-validity-in-seconds}") Long tokenValidityInSeconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliSeconds = tokenValidityInSeconds * 1000;
+    }
+
+//    빈 생성 후 의존성 주입 설정 후에 주입받은 secret 값을 Base64로 Decode한 후 key 변수에 할당
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
