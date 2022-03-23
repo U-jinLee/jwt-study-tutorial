51cdc89a3e81b2c00c4cfaeec1ada12f592965da
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-03-23 23:10:04 +0900

    Fix: Filter의 확인방법 변경

--- src/main/java/me/silvernine/jwttutorial/jwt/JwtAuthenticationFilter.java
+++ src/main/java/me/silvernine/jwttutorial/jwt/JwtAuthenticationFilter.java
@@ -6,7 +6,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilter {
@@ -14,14 +16,34 @@ public class JwtAuthenticationFilter extends GenericFilter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 헤더에서 JWT를 받아온다.
+        String accessToken = jwtTokenProvider.resolveAccessToken((HttpServletRequest) request);
+        String refreshToken = jwtTokenProvider.resolveRefreshToken((HttpServletRequest) request);
+        if(accessToken != null) {
+            if(jwtTokenProvider.validateToken(accessToken)) {
+                this.setAuthentication(accessToken);
+            }
+        } else if(!jwtTokenProvider.validateToken(accessToken) && refreshToken != null) {
+            boolean validateRefreshToken = jwtTokenProvider.validateToken(refreshToken);
+            boolean isRefreshToken = jwtTokenProvider.existsRefreshToken(refreshToken);
 
-        if(token != null && jwtTokenProvider.validateToken(token)) {
-            //토큰이 유효할 시 토큰으로부터 유저 정보를 받아온다
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            //SecurityContext에 Authetication 객체 저장.
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if(validateRefreshToken && isRefreshToken) {
+                String email = jwtTokenProvider.getUserEmail(refreshToken);
+                List<String> roles = jwtTokenProvider.getRoles(email);
+                String newAccessToken = jwtTokenProvider.createAccessToken(email, roles);
+
+                jwtTokenProvider.setHeaderAccessToken((HttpServletResponse) response, newAccessToken);
+                this.setAuthentication(newAccessToken);
+            }
         }
         chain.doFilter(request, response);
+    }
+
+    public void setAuthentication(String token) {
+        //토큰으로 유저 정보를 받아온다.
+        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+        //SecurityContext에 Authentication 객체를 저장한다.
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
 }
