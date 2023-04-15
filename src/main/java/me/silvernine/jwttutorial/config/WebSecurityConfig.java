54671894672ff264664607be340b780b20c6d20b
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-04-16 00:08:25 +0900

    feat: Add security config

--- src/main/java/me/silvernine/jwttutorial/config/WebSecurityConfig.java
+++ src/main/java/me/silvernine/jwttutorial/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package me.silvernine.jwttutorial.config;
 
 import lombok.RequiredArgsConstructor;
+import me.silvernine.jwttutorial.jwt.JwtAccessDeniedHandler;
+import me.silvernine.jwttutorial.jwt.JwtAuthenticationEntryPoint;
 import me.silvernine.jwttutorial.jwt.JwtSecurityConfig;
 import me.silvernine.jwttutorial.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +24,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     // 회원가입 시 비밀번호 암호화를 위한 PasswordEncoder 빈 등록
     @Bean
@@ -36,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web
                 .ignoring()
                 .antMatchers("/h2-console/**", "/favicon.ico");
@@ -45,22 +49,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // csrf 비활성화
-                //Exception 처리를 위한 부분
-//                .exceptionHandling()
-//                .authenticationEntryPoint()
-//                .accessDeniedHandler()
-//                .and()
+                //jwt는 필요가 없으므로 csrf 비활성화
+                .csrf().disable()
+
                 //세션 사용하지 않음
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
                 .and()
+
+                //Exception 처리를 위한 부분
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
+
                 .authorizeRequests() //ServletRequest를 사용하는 요청에 대한 접근 제한 설정
                 .antMatchers(HttpMethod.POST, "/api/auth/**").permitAll() //검증 없이 이용가능(Post 요청 가능)
                 .anyRequest().authenticated()
-                //JwtSecurityConfig 적용
                 .and()
+
+                //JwtSecurityConfig 적용
                 .apply(new JwtSecurityConfig(jwtTokenProvider));
 
     }
